9fc130e7a77db17e645b864b4c008b846fe15329
mohamed tarek <mohamed.tarek72417293@gmail.com>
2024-02-20 21:42:58 +0200

    add securityFilterChain method

--- src/main/java/com/SpringBootMVC/ExpensesTracker/security/SecurityConfig.java
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/security/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.SpringBootMVC.ExpensesTracker.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -20,6 +22,25 @@ public class SecurityConfig {
         auth.setUserDetailsService(userService);
         auth.setPasswordEncoder(passwordEncoder());
         return auth;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(config ->
+                config
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
+                        .requestMatchers("/js/**").permitAll()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/showRegistrationForm").permitAll()
+                        .requestMatchers("/processRegistration").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form ->
+                        form
+                                .loginPage("/showLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll());
+        return http.build();
     }
 
 }
--- src/main/java/com/SpringBootMVC/ExpensesTracker/service/RoleServiceImpl.java
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/service/RoleServiceImpl.java
@@ -3,7 +3,9 @@ package com.SpringBootMVC.ExpensesTracker.service;
 import com.SpringBootMVC.ExpensesTracker.entity.Role;
 import com.SpringBootMVC.ExpensesTracker.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RoleServiceImpl implements RoleService {
 
     RoleRepository roleRepository;
