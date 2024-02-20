4ad09108ca0857059228b95ec816c0da6197d896
mohamed tarek <mohamed.tarek72417293@gmail.com>
2024-02-20 21:44:00 +0200

    add save method to save webUser to user and client to DB

--- src/main/java/com/SpringBootMVC/ExpensesTracker/DTO/WebUser.java
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/DTO/WebUser.java
@@ -1,10 +1,23 @@
 package com.SpringBootMVC.ExpensesTracker.DTO;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class WebUser {
+    @NotNull(message = "this field is required!")
+    @Size(min=1, message="this field is required")
     private String username;
+    @NotNull(message = "this field is required!")
+    @Size(min=1, message="this field is required")
     private String password;
+    @NotNull(message = "this field is required!")
+    @Size(min=1, message="this field is required")
     private String firstName;
+    @NotNull(message = "this field is required!")
+    @Size(min=1, message="this field is required")
     private String lastName;
+    @NotNull(message = "this field is required!")
+    @Size(min=1, message="this field is required")
     private String email;
 
     public WebUser() {
