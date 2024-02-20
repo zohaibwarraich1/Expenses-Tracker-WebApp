f840864d5241d4225d80912142e73d336ba9db09
mohamed tarek <mohamed.tarek72417293@gmail.com>
2024-02-20 17:55:36 +0200

    modify UserService to extends UserDetailsService instead of UserServiceImpl implements UserDetailsService.

--- src/main/java/com/SpringBootMVC/ExpensesTracker/service/UserService.java
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/service/UserService.java
@@ -1,7 +1,8 @@
 package com.SpringBootMVC.ExpensesTracker.service;
 
 import com.SpringBootMVC.ExpensesTracker.entity.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User findUserByUserName(String username);
 }
--- src/main/java/com/SpringBootMVC/ExpensesTracker/service/UserServiceImpl.java
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/service/UserServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 @Service
-public class UserServiceImpl implements UserService, UserDetailsService {
+public class UserServiceImpl implements UserService {
 
     UserRepository userRepository;
     @Autowired
