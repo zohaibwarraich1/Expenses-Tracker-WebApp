beceb10304620ac2dd28d139ee5c7a67d0b4aa89
mohamed tarek <mohamed.tarek72417293@gmail.com>
2024-02-20 18:20:19 +0200

    create SecurityConfig with passwordEncoder and DaoAuthenticaitonProvider

---
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.SpringBootMVC.ExpensesTracker.security;
+
+import com.SpringBootMVC.ExpensesTracker.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserService userService){
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+
+}
