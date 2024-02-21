d902e5154cd2dc2333bbfa47e0998243e5b40a19
mohamed tarek <mohamed.tarek72417293@gmail.com>
2024-02-21 11:39:04 +0200

    add save ExpenseDTO -> Expense. and save to DB

--- src/main/java/com/SpringBootMVC/ExpensesTracker/service/ExpenseService.java
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/service/ExpenseService.java
@@ -1,7 +1,9 @@
 package com.SpringBootMVC.ExpensesTracker.service;
 
+import com.SpringBootMVC.ExpensesTracker.DTO.ExpenseDTO;
 import com.SpringBootMVC.ExpensesTracker.entity.Expense;
 
 public interface ExpenseService {
     Expense findExpenseById(int id);
+    void save(ExpenseDTO expenseDTO);
 }
--- src/main/java/com/SpringBootMVC/ExpensesTracker/service/ExpenseServiceImpl.java
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/service/ExpenseServiceImpl.java
@@ -1,21 +1,43 @@
 package com.SpringBootMVC.ExpensesTracker.service;
 
+import com.SpringBootMVC.ExpensesTracker.DTO.ExpenseDTO;
+import com.SpringBootMVC.ExpensesTracker.entity.Category;
 import com.SpringBootMVC.ExpensesTracker.entity.Expense;
 import com.SpringBootMVC.ExpensesTracker.repository.ExpenseRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExpenseServiceImpl implements ExpenseService {
     ExpenseRepository expenseRepository;
+    ClientService clientService;
+    CategoryService categoryService;
 
     @Autowired
-    public ExpenseServiceImpl(ExpenseRepository expenseRepository) {
+    public ExpenseServiceImpl(ExpenseRepository expenseRepository, ClientService clientService, CategoryService categoryService) {
         this.expenseRepository = expenseRepository;
+        this.clientService = clientService;
+        this.categoryService = categoryService;
     }
+
 
     @Override
     public Expense findExpenseById(int id) {
         return expenseRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    @Override
+    public void save(ExpenseDTO expenseDTO) {
+        System.out.println(expenseDTO);
+        Expense expense = new Expense();
+        expense.setAmount(expenseDTO.getAmount());
+        expense.setDateTime(expenseDTO.getDateTime());
+        expense.setDescription(expenseDTO.getDescription());
+        expense.setClient(clientService.findClientById(expenseDTO.getClientId()));
+        Category category = categoryService.findCategoryByName(expenseDTO.getCategory());
+        expense.setCategory(category);
+        expenseRepository.save(expense);
     }
 }
